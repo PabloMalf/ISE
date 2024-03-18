a3a80c37314c0a73ef717a391350d6aabf3bf9f3
PabloMalf <89598259+PabloMalf@users.noreply.github.com>
2024-03-18 16:52:41 +0100

    B1 fin

--- B1/P2/P2_Ejer_1/Parpadeo.h
+++
@@ -1,5 +0,0 @@
-#ifndef __PARPADEO_H
-#define __PARPADEO_H
-int Init_Parpadeo (void);
-
-#endif
--- B1/P2/P2_Ejer_1/leds.h
+++
@@ -1,9 +0,0 @@
-#include "stdint.h"
-#include "stm32f4xx_hal.h"
-#include "cmsis_os2.h"
-
-#include "string.h"
-#include "stdio.h"
-
-/* Prototipo de funciones */
-void LED_Init(void);
