848ed71dcc2779bd4bed658af11bec7ceba051f0
LuisLopIzq <luis.lopez.izquierdo@alumnos.upm.es>
2024-05-09 20:26:28 +0200

    RTC con y sin SNTP

--- B2/Modulos/rtc/rtc.c
+++ B2/Modulos/rtc/rtc.c
@@ -26,17 +26,17 @@ static void Th_rtc(void *arg);
 int init_Th_rtc(void);
 
 // Functions
-void init_rtc(void);
-void init_SNTP(void);
-void RTC_CalendarConfig(uint8_t hour, uint8_t min, uint8_t sec,  uint8_t day, uint8_t month,  uint8_t year);
-void RTC_Show(void);
-void Init_timers (void);
+static void init_rtc(void);
+static void init_SNTP(void);
+static void RTC_CalendarConfig(void);
+static void RTC_Show(void);
+static void Init_timers (void);
 
 static void Timer_Callback_3min (void const *arg) {
 	 init_SNTP(); // Cada 3 min se llama al servidor SNTP
 }
 
-void Init_timers (void) {
+static void Init_timers (void) {
 	exec = 1U;
 	tim_id_3min = osTimerNew((osTimerFunc_t)&Timer_Callback_3min, osTimerPeriodic, &exec, NULL);
 
@@ -49,50 +49,14 @@ int init_Th_rtc(void){
 	return(0);
 }
 
-void init_rtc(void){
-  /*##-1- Configure the rtc peripheral #######################################*/
-  /* Configure rtc prescaler and rtc data registers */
-  /* rtc configured as follows:
-      - Hour Format    = Format 24
-      - Asynch Prediv  = Value according to source clock
-      - Synch Prediv   = Value according to source clock
-      - OutPut         = Output Disable
-      - OutPutPolarity = High Polarity
-      - OutPutType     = Open Drain */ 
-  RtcHandle.Instance = RTC; 
-  RtcHandle.Init.HourFormat = RTC_HOURFORMAT_24;
-  RtcHandle.Init.AsynchPrediv = RTC_ASYNCH_PREDIV;
-  RtcHandle.Init.SynchPrediv = RTC_SYNCH_PREDIV;
-  RtcHandle.Init.OutPut = RTC_OUTPUT_DISABLE;
-  RtcHandle.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
-  RtcHandle.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
-  __HAL_RTC_RESET_HANDLE_STATE(&RtcHandle);
-	
-  HAL_RTC_Init(&RtcHandle);
-  init_SNTP();
-	
-}
-
-
-/***Configuracion********/
 void HAL_RTC_MspInit(RTC_HandleTypeDef *hrtc)
 {
   RCC_OscInitTypeDef        RCC_OscInitStruct;
   RCC_PeriphCLKInitTypeDef  PeriphClkInitStruct;
 
-  /*##-1- Enables the PWR Clock and Enables access to the backup domain ###################################*/
-  /* To change the source clock of the RTC feature (LSE, LSI), You have to:
-     - Enable the power clock using __HAL_RCC_PWR_CLK_ENABLE()
-     - Enable write access using HAL_PWR_EnableBkUpAccess() function before to 
-       configure the RTC clock source (to be done once after reset).
-     - Reset the Back up Domain using __HAL_RCC_BACKUPRESET_FORCE() and 
-       __HAL_RCC_BACKUPRESET_RELEASE().
-     - Configure the needed RTc clock source */
   __HAL_RCC_PWR_CLK_ENABLE();
   HAL_PWR_EnableBkUpAccess();
 
-  
-  /*##-2- Configure LSE as RTC clock source ###################################*/
   RCC_OscInitStruct.OscillatorType =  RCC_OSCILLATORTYPE_LSE;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   RCC_OscInitStruct.LSEState = RCC_LSE_ON;
@@ -103,23 +67,67 @@ void HAL_RTC_MspInit(RTC_HandleTypeDef *hrtc)
   PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
   HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
 
-  
-  /*##-3- Enable RTC peripheral Clocks #######################################*/
-  /* Enable RTC Clock */
   __HAL_RCC_RTC_ENABLE();
 }
 
 void HAL_RTC_MspDeInit(RTC_HandleTypeDef *hrtc)
 {
-  /*##-1- Reset peripherals ##################################################*/
   __HAL_RCC_RTC_DISABLE();
 
-  /*##-2- Disables the PWR Clock and Disables access to the backup domain ###################################*/
   HAL_PWR_DisableBkUpAccess();
   __HAL_RCC_PWR_CLK_DISABLE();
   
 }
-void RTC_Show()
+
+static void RTC_CalendarConfig(void)
+{
+
+  /*##-1- Configure the Date #################################################*/
+  /* Set Date: Tuesday February 18th 2014 */
+  sdatestructure.Year = -148;
+  sdatestructure.Month = RTC_MONTH_APRIL; //RTC_MONTH_FEBRUARY
+  sdatestructure.Date = 0x14;
+  sdatestructure.WeekDay = RTC_WEEKDAY_TUESDAY;
+  
+  HAL_RTC_SetDate(&RtcHandle,&sdatestructure,RTC_FORMAT_BCD);
+
+
+  /*##-2- Configure the Time #################################################*/
+  /* Set Time: 02:00:00 */
+  stimestructure.Hours = 0x23; //23
+  stimestructure.Minutes = 0x59; //59
+  stimestructure.Seconds = 0x00;
+  stimestructure.TimeFormat = RTC_HOURFORMAT12_PM;
+  stimestructure.DayLightSaving = RTC_DAYLIGHTSAVING_NONE ;
+  stimestructure.StoreOperation = RTC_STOREOPERATION_RESET;
+
+  HAL_RTC_SetTime(&RtcHandle, &stimestructure, RTC_FORMAT_BCD);
+
+
+  /*##-3- Writes a data in a RTC Backup data Register1 #######################*/
+  HAL_RTCEx_BKUPWrite(&RtcHandle, RTC_BKP_DR1, 0x32F2);
+}
+
+static void init_rtc(void){
+  RtcHandle.Instance = RTC; 
+  RtcHandle.Init.HourFormat = RTC_HOURFORMAT_24;
+  RtcHandle.Init.AsynchPrediv = RTC_ASYNCH_PREDIV;
+  RtcHandle.Init.SynchPrediv = RTC_SYNCH_PREDIV;
+  RtcHandle.Init.OutPut = RTC_OUTPUT_DISABLE;
+  RtcHandle.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+  RtcHandle.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+  __HAL_RTC_RESET_HANDLE_STATE(&RtcHandle);
+	
+  HAL_RTC_Init(&RtcHandle);
+	RTC_CalendarConfig();
+  init_SNTP();
+	
+}
+
+
+
+
+static void RTC_Show()
 {
   HAL_RTC_GetTime(&RtcHandle, &stimestructure, RTC_FORMAT_BIN);
   HAL_RTC_GetDate(&RtcHandle, &sdatestructure, RTC_FORMAT_BIN);
@@ -133,8 +141,7 @@ void RTC_Show()
 
 }
 
-/***SNTP***/
-void init_SNTP (void) {
+static void init_SNTP (void) {
   netStatus status = netSNTPc_GetTime (NULL, time_callback);
 }
 
@@ -157,35 +164,31 @@ static void time_callback (uint32_t seconds, uint32_t seconds_fraction) {
    
 	 HAL_RTC_SetTime(&RtcHandle, &stimestructure, RTC_FORMAT_BIN);
 	
-	   /*##-3- Writes a data in a RTC Backup data Register1 #######################*/
   HAL_RTCEx_BKUPWrite(&RtcHandle, RTC_BKP_DR1, 0x32F2);
   }
 	
-	else{
-	sdatestructure.Year = 4 ;
-  sdatestructure.Month = RTC_MONTH_MAY; //RTC_MONTH_FEBRUARY
-  sdatestructure.Date = 13;
-  sdatestructure.WeekDay = RTC_WEEKDAY_TUESDAY;
-  
-  HAL_RTC_SetDate(&RtcHandle,&sdatestructure,RTC_FORMAT_BCD);
+//	else{
+//	sdatestructure.Year = 4 ;
+//  sdatestructure.Month = RTC_MONTH_MAY;
+//  sdatestructure.Date = 13;
+//  sdatestructure.WeekDay = RTC_WEEKDAY_TUESDAY;
+//  
+//  HAL_RTC_SetDate(&RtcHandle,&sdatestructure,RTC_FORMAT_BCD);
 
 
-  /*##-2- Configure the Time #################################################*/
-  /* Set Time: 02:00:00 */
-  stimestructure.Hours = 3;
-  stimestructure.Minutes = 30;
-  stimestructure.Seconds = 3;
-  stimestructure.TimeFormat = RTC_HOURFORMAT12_AM;
-  stimestructure.DayLightSaving = RTC_DAYLIGHTSAVING_NONE ;
-  stimestructure.StoreOperation = RTC_STOREOPERATION_RESET;
+//  stimestructure.Hours = 3;
+//  stimestructure.Minutes = 30;
+//  stimestructure.Seconds = 3;
+//  stimestructure.TimeFormat = RTC_HOURFORMAT12_AM;
+//  stimestructure.DayLightSaving = RTC_DAYLIGHTSAVING_NONE ;
+//  stimestructure.StoreOperation = RTC_STOREOPERATION_RESET;
 
-  HAL_RTC_SetTime(&RtcHandle, &stimestructure, RTC_FORMAT_BCD);
+//  HAL_RTC_SetTime(&RtcHandle, &stimestructure, RTC_FORMAT_BCD);
 
 
-  /*##-3- Writes a data in a RTC Backup data Register1 #######################*/
-  HAL_RTCEx_BKUPWrite(&RtcHandle, RTC_BKP_DR1, 0x32F2);
-		
-	}
+//  HAL_RTCEx_BKUPWrite(&RtcHandle, RTC_BKP_DR1, 0x32F2);
+//		
+	//}
 }
 
 
