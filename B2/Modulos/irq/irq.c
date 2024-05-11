ba47059763797d2352991cbc5677c6c289d8a7f8
PabloMalf <89598259+PabloMalf@users.noreply.github.com>
2024-05-11 22:55:16 +0200

    Arreglo IRQ + NFC

--- B2/Modulos/irq/irq.c
+++ B2/Modulos/irq/irq.c
@@ -1,3 +1,18 @@
 #include "irq.h"
 
-//PLACEHOLDER
+#define FLAG_IRQ				1U << 2
+
+extern uint8_t teclapulsada;
+extern osThreadId_t id_Th_key;
+
+void EXTI9_5_IRQHandler(void){
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_6);
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_5);
+}
+
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+	if(teclapulsada==0)
+	osThreadFlagsSet(id_Th_key, FLAG_IRQ);
+}
--- B2/Modulos/irq/irq.h
+++ B2/Modulos/irq/irq.h
@@ -1,9 +1,10 @@
 #ifndef __IRQ_H
 #define __IRQ_H
 
-#include "cmsis_os.h"
+#include "cmsis_os2.h"
 #include "stm32f4xx_hal.h"
 
-//PLACEHOLDER
+void EXTI9_5_IRQHandler(void);
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
 
 #endif
