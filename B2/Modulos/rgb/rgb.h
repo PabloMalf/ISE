a25603dc4e43c0b9adf37b2f7f1da288fce25364
PabloMalf <89598259+PabloMalf@users.noreply.github.com>
2024-05-12 12:26:48 +0200

    Update Interfaz

--- B2/Modulos/rgb/rgb.h
+++ B2/Modulos/rgb/rgb.h
@@ -7,16 +7,12 @@
 /*
 			TIM1
 			PINOUT USED:
-			TIM1_CH1	->	PE9
-			TIM1_CH2	->	PE11
-			TIM1_CH3	->	PE13
+			TIM1_CH1	->	PE9   -> R
+			TIM1_CH2	->	PE11  -> G
+			TIM1_CH3	->	PE13  -> B
 */
 
-typedef struct{ 
-	uint8_t r;
-	uint8_t g;
-	uint8_t b;
-} MSGQUEUE_OBJ_RGB;
+typedef uint8_t MSGQUEUE_OBJ_RGB[3];
 
 int init_Th_rgb(void);
 osMessageQueueId_t get_id_MsgQueue_rgb(void);
